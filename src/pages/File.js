d52909bd56de18a24aa9fa3799b2394938d7680b
Bashkim Durmishi <35192061+prelium@users.noreply.github.com>
2021-09-22 22:39:17 +0200

    changed request headers

--- src/pages/File.js
+++ src/pages/File.js
@@ -1,154 +1,190 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import ProgressBar from '../components/progressbar/ProgressBar';
-import Countdown from 'react-countdown';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import ProgressBar from "../components/progressbar/ProgressBar";
+import Countdown from "react-countdown";
 
-import { StyledDropZone } from 'react-drop-zone';
-import 'react-drop-zone/dist/styles.css';
-
+import { StyledDropZone } from "react-drop-zone";
+import "react-drop-zone/dist/styles.css";
 
 function File() {
-    const [fileId, setFileId] = useState("");
-    const [data, setData] = useState(null);
-    const [stats, setStats] = useState(null);
-    const [refresh, setRefresh] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [fileId, setFileId] = useState("");
+  const [data, setData] = useState(null);
+  const [stats, setStats] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const baseUrl = "https://www.virustotal.com/api/v3";
+  const baseUrl = "https://www.virustotal.com/api/v3";
 
-    const instance = axios.create({
-        baseURL: baseUrl,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+  const instance = axios.create({
+    baseURL: baseUrl,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  instance.defaults.headers.get["x-apikey"] =
+    process.env.REACT_APP_VIRUS_TOTAL_KEY;
+  instance.defaults.headers.post["x-apikey"] =
+    process.env.REACT_APP_VIRUS_TOTAL_KEY;
+
+  instance.defaults.headers.get["Content-Type"] =
+    "application/x-www-form-urlencoded";
+  instance.defaults.headers.post["Content-Type"] =
+    "application/x-www-form-urlencoded";
+
+  const onDrop = (file) => {
+    setData(null);
+    setLoading(true);
+
+    let formData = new FormData();
+    formData.append("file", file);
+
+    instance
+      .post(`/files`, formData)
+      .then((response) => {
+        if (response.status >= 200 || response.status <= 299) {
+          setFileId(response.data.data.id);
+          getAnalasisResult(response.data.data.id);
         }
-    })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    instance.defaults.headers.get['x-apikey'] = process.env.REACT_APP_VIRUS_TOTAL_KEY;
-    instance.defaults.headers.post['x-apikey'] = process.env.REACT_APP_VIRUS_TOTAL_KEY;
-    
-    const onDrop = (file) => {
-        setData(null);
-        setLoading(true);
+  const getAnalasisResult = (scannedFileId) => {
+    instance
+      .get(`/analyses/${scannedFileId}`)
+      .then((response) => {
+        if (response.status >= 200 || response.status <= 299) {
+          if (response.data.data.attributes.status === "queued") {
+            setRefresh(true);
+            setLoading(false);
+          } else {
+            let stats = response.data.data.attributes.stats;
+            // Sum of the results (harmless, malicius, suspicious, etc)
+            stats.sum = Object.values(stats).reduce((a, b) => a + b, 0);
 
-        let formData = new FormData();
-        formData.append("file", file);
-
-        instance.post(`/files`, formData).then(response => {
-            if (response.status >= 200 || response.status <= 299){
-                setFileId(response.data.data.id);
-                getAnalasisResult(response.data.data.id);
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-    }
-
-    const getAnalasisResult = (scannedFileId) => {
-        instance.get(`/analyses/${scannedFileId}`).then(response => {
-            if (response.status >= 200 || response.status <= 299){
-                if(response.data.data.attributes.status === "queued") {
-                    setRefresh(true);
-                    setLoading(false);
-                } else {
-                    let stats = response.data.data.attributes.stats;
-                    // Sum of the results (harmless, malicius, suspicious, etc)
-                    stats.sum = Object.values(stats).reduce((a,b) => a+b, 0);
-
-                    setStats(stats);
-                    setData(response.data.data);
-                    setLoading(false);
-                }
-                return response.data;
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-    }
-
-    const renderLoading = () => {
-        return (
-            <TableRow key={"no-data"}>
-                <TableCell component="td" colSpan={3} scope="row">Loading results...</TableCell>
-            </TableRow>
-        )
-    }
-    
-    const renderAnalysing = () => {
-        return (
-            <TableRow key={"analysing-data"}>
-                <TableCell component="td" colSpan={3} scope="row">Analysing file... File is in queue, please wait and press the refresh button after 10 seconds.</TableCell>
-            </TableRow>
-        )
-    }
-
-    const renderData = () => {
-        let tableRows = [];
-
-        if(data) {
-            for (const [key, value] of Object.entries(data.attributes.results)) {
-                tableRows.push(
-                    <TableRow key={key}>
-                        <TableCell component="td" scope="row">
-                            {key}
-                        </TableCell>
-                        <TableCell align="right">{value.category}</TableCell>
-                    </TableRow>
-                )
-            }
+            setStats(stats);
+            setData(response.data.data);
+            setLoading(false);
+          }
+          return response.data;
         }
-        return tableRows;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const renderLoading = () => {
+    return (
+      <TableRow key={"no-data"}>
+        <TableCell component="td" colSpan={3} scope="row">
+          Loading results...
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  const renderAnalysing = () => {
+    return (
+      <TableRow key={"analysing-data"}>
+        <TableCell component="td" colSpan={3} scope="row">
+          Analysing file... File is in queue, please wait and press the refresh
+          button after 10 seconds.
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  const renderData = () => {
+    let tableRows = [];
+
+    if (data) {
+      for (const [key, value] of Object.entries(data.attributes.results)) {
+        tableRows.push(
+          <TableRow key={key}>
+            <TableCell component="td" scope="row">
+              {key}
+            </TableCell>
+            <TableCell align="right">{value.category}</TableCell>
+          </TableRow>
+        );
+      }
     }
+    return tableRows;
+  };
 
-    const renderTable = () => {
-        return (
-            <TableContainer className="mt-4">
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Engine</TableCell>
-                            <TableCell align="right">Category</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {refresh && renderAnalysing()}
-                        {loading && renderLoading()}
-                        {data && renderData()}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        )
-    }
+  const renderTable = () => {
+    return (
+      <TableContainer className="mt-4">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Engine</TableCell>
+              <TableCell align="right">Category</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {refresh && renderAnalysing()}
+            {loading && renderLoading()}
+            {data && renderData()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
 
-    const Completionist = () => <button className="btn btn-primary mt-3" onClick={() => {getAnalasisResult(fileId); setRefresh(false);}}>Refresh</button>;
+  const Completionist = () => (
+    <button
+      className="btn btn-primary mt-3"
+      onClick={() => {
+        getAnalasisResult(fileId);
+        setRefresh(false);
+      }}
+    >
+      Refresh
+    </button>
+  );
 
-    return(
-        <div className="main-section mx-3 position-relative">
-            {stats && <ProgressBar
-                key={"progressbar"}
-                malicious={stats.malicious}
-                scans={stats.sum}
-                size={100}
-                strokeWidth={10}
-                circleTwoStroke="red"
-            />}
-            <h2>FILE SCAN</h2>
-            <StyledDropZone onDrop={(file) => {onDrop(file)}} className="file-input text-white" style={{minHeight: '50px'}} />
-            {refresh && (
-            <h2 className="mt-2">
-                <Countdown className="text-" date={Date.now() + 10000}>
-                    <Completionist />
-                </Countdown>
-            </h2>)}
-            {renderTable()}
-        </div>
-    )
+  return (
+    <div className="main-section mx-3 position-relative">
+      {stats && (
+        <ProgressBar
+          key={"progressbar"}
+          malicious={stats.malicious}
+          scans={stats.sum}
+          size={100}
+          strokeWidth={10}
+          circleTwoStroke="red"
+        />
+      )}
+      <h2>FILE SCAN</h2>
+      <StyledDropZone
+        onDrop={(file) => {
+          onDrop(file);
+        }}
+        className="file-input text-white"
+        style={{ minHeight: "50px" }}
+      />
+      {refresh && (
+        <h2 className="mt-2">
+          <Countdown className="text-" date={Date.now() + 10000}>
+            <Completionist />
+          </Countdown>
+        </h2>
+      )}
+      {renderTable()}
+    </div>
+  );
 }
 
 export default File;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,127 +1,152 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import ProgressBar from '../components/progressbar/ProgressBar';
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import ProgressBar from "../components/progressbar/ProgressBar";
 
 function Home() {
-    const [url, setUrl] = useState("");
-    const [data, setData] = useState(null);
-    const [stats, setStats] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState(null);
+  const [stats, setStats] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const baseUrl = "https://www.virustotal.com/api/v3";
+  const baseUrl = "https://www.virustotal.com/api/v3";
 
-    const instance = axios.create({
-        baseURL: baseUrl,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+  const instance = axios.create({
+    baseURL: baseUrl,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  instance.defaults.headers.get["x-apikey"] =
+    process.env.REACT_APP_VIRUS_TOTAL_KEY;
+  instance.defaults.headers.get["Content-Type"] =
+    "application/x-www-form-urlencoded";
+  instance.defaults.headers.post["Content-Type"] =
+    "application/x-www-form-urlencoded";
+  instance.defaults.headers.post["x-apikey"] =
+    process.env.REACT_APP_VIRUS_TOTAL_KEY;
+
+  const handleSubmit = (e) => {
+    setData(null);
+    e.preventDefault();
+    setLoading(true);
+    var encodedUrl = btoa(url);
+    encodedUrl = encodedUrl.split("=")[0];
+    console.log(encodedUrl);
+    instance
+      .get(`/urls/${encodedUrl}`)
+      .then((response) => {
+        if (response.status >= 200 || response.status <= 299) {
+          let stats = response.data.data.attributes.last_analysis_stats;
+          // Sum of the results (harmless, malicius, suspicious, etc)
+          stats.sum = Object.values(stats).reduce((a, b) => a + b, 0);
+
+          setLoading(false);
+          setData(response.data.data);
+          setStats(stats);
         }
-    })
+      })
+      .catch((err) => {
+        setLoading(false);
+        setData("error");
+        console.log(err);
+      });
+  };
 
-    instance.defaults.headers.get['x-apikey'] = process.env.REACT_APP_VIRUS_TOTAL_KEY;
-    instance.defaults.headers.post['x-apikey'] = process.env.REACT_APP_VIRUS_TOTAL_KEY;
+  const renderLoading = () => {
+    return (
+      <TableRow key={"no-data"}>
+        <TableCell component="td" colSpan={3} scope="row">
+          Loading results...
+        </TableCell>
+      </TableRow>
+    );
+  };
 
-    const handleSubmit = (e) => {
-        setData(null);
-        e.preventDefault();
-        setLoading(true);
-        var encodedUrl = btoa(url);
-        encodedUrl = encodedUrl.split("=")[0];
-        console.log(encodedUrl);
-        instance.get(`/urls/${encodedUrl}`).then(response => {
-            if (response.status >= 200 || response.status <= 299){
-                let stats = response.data.data.attributes.last_analysis_stats;
-                // Sum of the results (harmless, malicius, suspicious, etc)
-                stats.sum = Object.values(stats).reduce((a,b) => a+b, 0);
-                
-                setLoading(false);
-                setData(response.data.data);
-                setStats(stats);
-            }
-        }).catch(err => {
-            setLoading(false);
-            setData("error");
-            console.log(err);
-        });
+  const renderData = () => {
+    let tableRows = [];
+
+    if (data) {
+      for (const [key, value] of Object.entries(
+        data.attributes.last_analysis_results
+      )) {
+        tableRows.push(
+          <TableRow key={key}>
+            <TableCell component="td" scope="row">
+              {key}
+            </TableCell>
+            <TableCell align="right">{value.category}</TableCell>
+            <TableCell align="right">{value.result}</TableCell>
+          </TableRow>
+        );
+      }
     }
+    return tableRows;
+  };
 
-    const renderLoading = () => {
-        return (
-            <TableRow key={"no-data"}>
-                <TableCell component="td" colSpan={3} scope="row">Loading results...</TableCell>
+  const renderTable = () => {
+    return (
+      <TableContainer className="mt-4">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Engine</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Result</TableCell>
             </TableRow>
-        )
-    }
+          </TableHead>
+          <TableBody>
+            {loading && renderLoading()}
+            {data && renderData()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
 
-    const renderData = () => {
-        let tableRows = [];
-
-        if(data) {
-            for (const [key, value] of Object.entries(data.attributes.last_analysis_results)) {
-                tableRows.push(
-                    <TableRow key={key}>
-                        <TableCell component="td" scope="row">
-                            {key}
-                        </TableCell>
-                        <TableCell align="right">{value.category}</TableCell>
-                        <TableCell align="right">{value.result}</TableCell>
-                    </TableRow>
-                )
-            }
-        }
-        return tableRows;
-    }
-
-    const renderTable = () => {
-        return (
-            <TableContainer className="mt-4">
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Engine</TableCell>
-                            <TableCell align="right">Category</TableCell>
-                            <TableCell align="right">Result</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {loading && renderLoading()}
-                        {data && renderData()}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        )
-    }
-
-    return(
-        <div className="main-section mx-3">
-            {stats && <ProgressBar
-                className="align-self-start"
-                key={"progressbar"}
-                malicious={stats.malicious}
-                scans={stats.sum}
-                size={100}
-                strokeWidth={10}
-                circleTwoStroke="red"
-            />}
-            <div className="d-flex flex-column align-items-center">
-                <h2>URL SCAN</h2>
-                <form className="d-flex" onSubmit={(e) => {handleSubmit(e)}}>
-                    <input className="form-control" type="text" value={url} placeholder="Eg: google.com" onChange={(e) => {
-                        setUrl(e.target.value);
-                        }} />
-                    <button className="btn btn-primary mx-2">Scan</button>
-                </form>
-            </div> 
-            {renderTable()}
-        </div>
-    )
+  return (
+    <div className="main-section mx-3">
+      {stats && (
+        <ProgressBar
+          className="align-self-start"
+          key={"progressbar"}
+          malicious={stats.malicious}
+          scans={stats.sum}
+          size={100}
+          strokeWidth={10}
+          circleTwoStroke="red"
+        />
+      )}
+      <div className="d-flex flex-column align-items-center">
+        <h2>URL SCAN</h2>
+        <form
+          className="d-flex"
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            className="form-control"
+            type="text"
+            value={url}
+            placeholder="Eg: google.com"
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+          />
+          <button className="btn btn-primary mx-2">Scan</button>
+        </form>
+      </div>
+      {renderTable()}
+    </div>
+  );
 }
 
 export default Home;
